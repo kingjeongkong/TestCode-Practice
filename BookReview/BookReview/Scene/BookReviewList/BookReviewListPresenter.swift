8da0049c86c531645dbb491e682cc6c8a519cbce
kingjeongkong <wjdqls9223@gmail.com>
2024-09-02 19:28:17 +0900

    프로젝트 세팅

---
+++ BookReview/BookReview/Scene/BookReviewList/BookReviewListPresenter.swift
@@ -0,0 +1,12 @@
+//
+//  BookReviewListPresenter.swift
+//  BookReview
+//
+//  Created by 왕정빈 on 9/2/24.
+//
+
+import Foundation
+
+final class BookReviewListPresenter {
+    
+}
---
+++ BookReview/BookReview/Scene/BookReviewList/BookReviewListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  BookReviewListViewController.swift
+//  BookReview
+//
+//  Created by 왕정빈 on 9/2/24.
+//
+
+import UIKit
+
+final class BookReviewListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
