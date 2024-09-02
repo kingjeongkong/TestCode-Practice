aa0bb893f458fedf03a8361875f0163cd2ef7556
kingjeongkong <wjdqls9223@gmail.com>
2024-09-03 02:07:35 +0900

    메인 화면 UI 구현

--- BookReview/BookReview/Scene/BookReviewList/BookReviewListPresenter.swift
+++ BookReview/BookReview/Scene/BookReviewList/BookReviewListPresenter.swift
@@ -5,8 +5,38 @@
 //  Created by 왕정빈 on 9/2/24.
 //
 
-import Foundation
+import UIKit
 
-final class BookReviewListPresenter {
+protocol BookReviewListProtocol {
+    func setupNavigationBar()
+    func setupViews()
+}
+
+final class BookReviewListPresenter: NSObject {
+    private let delegate: BookReviewListProtocol
+    
+    init(delegate: BookReviewListProtocol) {
+        self.delegate = delegate
+    }
+    
+    func viewDidLoad() {
+        delegate.setupNavigationBar()
+        delegate.setupViews()
+    }
+}
+
+extension BookReviewListPresenter: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: nil)
+        var content = cell.defaultContentConfiguration()
+        content.text = "\(indexPath)"
+        cell.contentConfiguration = content
+        return cell
+    }
+    
     
 }
--- BookReview/BookReview/Scene/BookReviewList/BookReviewListViewController.swift
+++ BookReview/BookReview/Scene/BookReviewList/BookReviewListViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import SnapKit
 
 final class BookReviewListViewController: UIViewController {
 
+    private lazy var presenter = BookReviewListPresenter(delegate: self)
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.dataSource = presenter
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        presenter.viewDidLoad()
     }
-
-
 }
 
+extension BookReviewListViewController: BookReviewListProtocol {
+    func setupNavigationBar() {
+        navigationItem.title = "도서 리뷰"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: nil)
+        navigationItem.rightBarButtonItem = rightBarButtonItem
+    }
+    
+    func setupViews() {
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
